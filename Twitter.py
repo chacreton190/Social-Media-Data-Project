951d59531fef928c0305e51adaad1056582c5848
chacreton190 <56094812+chacreton190@users.noreply.github.com>
2019-12-19 13:10:56 -0500

    k

--- Twitter.py
+++ Twitter.py
@@ -2,13 +2,26 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import Twitter_creds
+from datetime import date
+import pathlib
+from pathlib import Path
+import os
+
 
 class StdOutListener(StreamListener):
     def on_data(self, raw_data): #takes in data that is collected from StreamListener
+        Tweets_folder = Path("Twitter Results")
+        if Tweets_folder.exists() == False:
+            Tweets_folder.mkdir()
+        cwd = os.getcwd()
+        outfile = open(cwd + f"\\Twitter Results\\Tweets{date.today()}.csv", "w")
+        outfile.write(raw_data)
         print(raw_data)
         return True
+
     def on_error(self, status_code): #
         print(status_code)
+
 
 if __name__ == "__main__":
     listener = StdOutListener()
@@ -18,3 +31,5 @@ if __name__ == "__main__":
     stream = Stream(auth, listener)
 # Need to add an option to pass in the search terms as an input from the user
     stream.filter(track=["drunk", "Weed",])
+
+
